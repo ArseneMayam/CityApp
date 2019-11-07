8c3dbf9eb3b983294c760cd02b70d99430d449ef
ArseneMayam <mayamarsene@yahoo.com>
2019-11-07 15:35:52 -0500

    Inject dependency for dbContext

--- CityInfo.API/Entities/CityInfoContext.cs
+++ CityInfo.API/Entities/CityInfoContext.cs
@@ -8,7 +8,18 @@ namespace CityInfo.API.Entities
 {
     public class CityInfoContext : DbContext
     {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options)
+        {
+
+        }
         public DbSet<City> Cities { get; set; }
         public DbSet<PointOfInterest> PointsOfInterest { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("connectionstring");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
