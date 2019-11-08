812716589c6cd9a8d8ebafef1cfa09ce4d5268b3
ArseneMayam <mayamarsene@yahoo.com>
2019-11-08 10:14:00 -0500

    Edit point of interest entity

--- CityInfo.API/Migrations/20191108151142_CityInfoDBPOInterestMigration.Designer.cs
+++ CityInfo.API/Migrations/20191108151142_CityInfoDBPOInterestMigration.Designer.cs
@@ -3,14 +3,16 @@ using CityInfo.API.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CityInfo.API.Migrations
 {
     [DbContext(typeof(CityInfoContext))]
-    partial class CityInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20191108151142_CityInfoDBPOInterestMigration")]
+    partial class CityInfoDBPOInterestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
